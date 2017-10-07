9cc2d130794f4350de65a6fbfaa3eb85f6c7b570
jlynchpitt <joelynch523@gmail.com>
2017-10-07 13:53:20 -0400

    Basic file structure + sample database testing code

---
+++ src/MTR/NorthShoreExtension/mainMTR.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package MTR.NorthShoreExtension;
+
+/**
+ * @author Joe Lynch
+ * 
+ * SQL login user: root password: mtr1234 TODO: move this somewhere else
+ *
+ */
+public class mainMTR {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
