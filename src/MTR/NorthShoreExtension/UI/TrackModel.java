93e1a64ab29e673cc36e6b674aefaf97d7cbe198
erinlhiggins <elh76@pitt.edu>
2017-10-11 11:31:15 -0400

    adding the break functionality

--- src/MTR/NorthShoreExtension/UI/TrackModel.java
+++ src/MTR/NorthShoreExtension/UI/TrackModel.java
@@ -22,6 +22,7 @@ public class TrackModel extends JPanel implements MouseListener {
 		String heater;
 	}
 	//read in from db here
+    int x1, x2, x3, x4, y1, y2, y3, y4;
 	String status = "Free";
 	Boolean clicked = false;
 	Boolean failure = false;
@@ -91,10 +92,6 @@ public class TrackModel extends JPanel implements MouseListener {
 		      g.drawLine(450, 210, 400, 220);
 		      g.drawLine(510, 200, 600, 200);
 		      g.drawLine(610, 200, 700, 200);
-		      if(failure) {
-		    	  	g.drawLine(10, 10, 20, 20);
-		    	  	g.drawLine(10, 20, 20, 10);
-		      }
 		      g.drawArc(671, 200, 75, 95, 0, 90);
 		      g.setColor(Color.green);
 		      g.fillOval(460, 185, 10, 10);
@@ -103,6 +100,12 @@ public class TrackModel extends JPanel implements MouseListener {
 		      g.setColor(Color.black);
 		      g.drawRoundRect(870, 20, 300, 150, 90, 90);
 		      g.drawString("Inputs for Individual Testing:", 900, 40);
+		      
+		      if(failure) {
+				g.setColor(Color.red);
+		    	  	g.drawLine(x1, y1, x2, y2);
+		    	  	g.drawLine(x1, y2, x2, y1);
+		      }
 		      
 		   }
 		   
@@ -115,106 +118,108 @@ public class TrackModel extends JPanel implements MouseListener {
 	            int y=e.getY();
 	            //note in final version will just add the row because will be preloaded
 	            //green lines
-	            System.out.println(x+" "+y);
 	            if((x>=580 && x<=600) && (y>=64 && y<=120)) {
+	            		x1=584;
+	            		x2=594;
+	            		y1=65;
+	            		y2=75;
 	            		clicked = true;
 	            		colString = Arrays.toString(colNames).replaceAll("\\[|\\]|,|\\s", "  ");
 	            		dataString = "  Green/"+status+"           55/3                A           1           100             0.5                     55                                              0.5                        0.5                                       off";
-	            		
-	            		if((x>=550 && x<=630) && (y>=550 && y<=580)) {
-	            			failure = true;
-	            			
-	            		}
 	            		repaint();
 	            }
 	            else if((x>=580 && x<=600) && (y>=135 && y<=218)) {
+		            	x1=584;
+	            		x2=594;
+	            		y1=136;
+	            		y2=146;
 	            		clicked = true;
 	            		colString = Arrays.toString(colNames).replaceAll("\\[|\\]|,|\\s", "  ");
 	            		dataString = "  Green/"+status+"          55/3                A           2           100              1                      55                     PIONEER                  1                         1.5                                       off";
-	            		
-	            		if((x>=550 && x<=630) && (y>=550 && y<=580)) {
-	            			failure = true;
-	            			
-	            		}
+	
 	            		repaint();
 	            }
 	            else if((x>=580 && x<=600) && (y>=236 && y<=318)) {
+		            	x1=584;
+	            		x2=594;
+	            		y1=237;
+	            		y2=247;
 	            		clicked = true;
 	            		colString = Arrays.toString(colNames).replaceAll("\\[|\\]|,|\\s", "  ");
 	            		dataString = "  Green/"+status+"           55/3                A           3           100             1.5                     55                                              1.5                         3                                       off";
-	            	
-	            		if((x>=550 && x<=630) && (y>=550 && y<=580)) {
-	            			failure = true;
-	            			
-	            		}
+	
 	            		repaint();
                 }
 	            //red lines
 	            else if((y>=190 && y<=210) && (x>=398 && x<=447)) {
+		            	x1=419;
+	            		x2=429;
+	            		y1=170;
+	            		y2=180;
 	            		clicked = true;
 	            		colString = Arrays.toString(colNames).replaceAll("\\[|\\]|,|\\s", "  ");
 	            		dataString = "  Red/"+status+"              55/3                A           1            50             0.5                     40                                              0.25                        0.25                                       off";
-	            	
-	            		if((x>=550 && x<=630) && (y>=550 && y<=580)) {
-	            			failure = true;
-	            			
-	            		}
+	  
 	            		repaint();
 	            }
 	            else if((y>=232 && y<=242) && (x>=398 && x<=447)) {
+		            	x1=419;
+	            		x2=429;
+	            		y1=212;
+	            		y2=222;
 	            		clicked = true;
 	            		colString = Arrays.toString(colNames).replaceAll("\\[|\\]|,|\\s", "  ");
 	            		dataString = "  Red/"+status+"              55/3                A           2            50              1                      40                                              0.50                        0.75                                       off";
-	            		
-	            		if((x>=550 && x<=630) && (y>=550 && y<=580)) {
-	            			failure = true;
-	            			
-	            		}
+	   
 	            		repaint();
 	            }
 	            else if((y>=220 && y<=270) && (x>=460 && x<=499)) {
+		            	x1=481;
+	            		x2=491;
+	            		y1=194;
+	            		y2=204;
 	            		clicked = true;
 	            		colString = Arrays.toString(colNames).replaceAll("\\[|\\]|,|\\s", "  ");
 	            		dataString = "  Red/"+status+"              55/3                A           3            50             1.5                     40                      SWITCH             0.75                      1.50                                       off";
-	            	
-	            		if((x>=550 && x<=630) && (y>=550 && y<=580)) {
-	            			failure = true;
-	            			
-	            		}
+	
 	            		repaint();
 	            }
 	            else if((y>=220 && y<=270) && (x>=510 && x<=600)) {
+		            	x1=531;
+	            		x2=541;
+	            		y1=194;
+	            		y2=204;
 	            		clicked = true;
 	            		colString = Arrays.toString(colNames).replaceAll("\\[|\\]|,|\\s", "  ");
 	            		dataString = "  Red/"+status+"              55/3                B           4            50              2                      40                                              1.00                        2.50                                       off";
-	            	
-	            		if((x>=550 && x<=630) && (y>=550 && y<=580)) {
-	            			failure = true;
-	            			
-	            		}
+	
 	            		repaint();
 	            }
 	            else if((y>=220 && y<=270) && (x>=611 && x<=685)) {
+		            	x1=632;
+	            		x2=642;
+	            		y1=194;
+	            		y2=204;
 	            		clicked = true;
 	            		colString = Arrays.toString(colNames).replaceAll("\\[|\\]|,|\\s", "  ");
 	            		dataString = "  Red/"+status+"              55/3                B           5            50             1.5                     40                                              0.75                        3.25                                       off";
-	         
-	            		if((x>=550 && x<=630) && (y>=550 && y<=580)) {
-	            			failure = true;
-	            		
-	            		}
+
 	            		repaint();
 	            }
 	            else if((y>=220 && y<=270) && (x>=692 && x<=740)) {
+		            	x1=713;
+	            		x2=723;
+	            		y1=214;
+	            		y2=224;
 	            		clicked = true;
 	            		colString = Arrays.toString(colNames).replaceAll("\\[|\\]|,|\\s", "  ");
 	            		dataString = "  Red/"+status+"              55/3                B           6            50              1                      40                                              0.50                        3.75                                       off";
-	            		if((x>=550 && x<=630) && (y>=550 && y<=590)) {
-	            			failure = true;
-	            			
-	            		}
 	            		repaint();
 	            }
+	            else if((x>=550 && x<=630) && (y>=550 && y<=600)) {
+        				failure = !failure;
+        				repaint();
+        		    }
 	            
 	        }
 	        
