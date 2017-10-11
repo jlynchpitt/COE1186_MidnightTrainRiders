f6dd2fb5b40aedb18e33de2118add13fb7879558
erinlhiggins <elh76@pitt.edu>
2017-10-11 14:42:26 -0400

    reverting

--- src/MTR/NorthShoreExtension/UI/TrackModel.java
+++ src/MTR/NorthShoreExtension/UI/TrackModel.java
@@ -6,9 +6,6 @@ import javax.swing.*;
 import java.util.Arrays;
 
 public class TrackModel extends JPanel implements MouseListener {
-	//Specify the look and feel to use.  Valid values:
-    //null (use the default), "Metal", "System", "Motif", "GTK+"
-    final static String LOOKANDFEEL = "System";
 	//load csv into db
 	//test 
     class Data{
@@ -42,7 +39,6 @@ public class TrackModel extends JPanel implements MouseListener {
 	Object[][] data = {{"1","1","1","1","1","1","1","1","1","1","1","1"}};
 	
 	   public static void main(String[] a) {
-		      initLookAndFeel();
 		      TrackModel mouse = new TrackModel();
 		      JFrame f = new JFrame("Track Model UI");
 		      f.setSize(1200, 700);
@@ -51,45 +47,7 @@ public class TrackModel extends JPanel implements MouseListener {
 		      f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		      f.setVisible(true);
 		   }
-		private static void initLookAndFeel() {
-	        String lookAndFeel = null;
-
-	        if (LOOKANDFEEL != null) {
-	            if (LOOKANDFEEL.equals("Metal")) {
-	                lookAndFeel = UIManager.getCrossPlatformLookAndFeelClassName();
-	            } else if (LOOKANDFEEL.equals("System")) {
-	                lookAndFeel = UIManager.getSystemLookAndFeelClassName();
-	            } else if (LOOKANDFEEL.equals("Motif")) {
-	                lookAndFeel = "com.sun.java.swing.plaf.motif.MotifLookAndFeel";
-	            } else if (LOOKANDFEEL.equals("GTK+")) { //new in 1.4.2
-	                lookAndFeel = "com.sun.java.swing.plaf.gtk.GTKLookAndFeel";
-	            } else {
-	                System.err.println("Unexpected value of LOOKANDFEEL specified: "
-	                                   + LOOKANDFEEL);
-	                lookAndFeel = UIManager.getCrossPlatformLookAndFeelClassName();
-	            }
-
-	            try {
-	                UIManager.setLookAndFeel(lookAndFeel);
-	            } catch (ClassNotFoundException e) {
-	                System.err.println("Couldn't find class for specified look and feel:"
-	                                   + lookAndFeel);
-	                System.err.println("Did you include the L&F library in the class path?");
-	                System.err.println("Using the default look and feel.");
-	            } catch (UnsupportedLookAndFeelException e) {
-	                System.err.println("Can't use the specified look and feel ("
-	                                   + lookAndFeel
-	                                   + ") on this platform.");
-	                System.err.println("Using the default look and feel.");
-	            } catch (Exception e) {
-	                System.err.println("Couldn't get specified look and feel ("
-	                                   + lookAndFeel
-	                                   + "), for some reason.");
-	                System.err.println("Using the default look and feel.");
-	                e.printStackTrace();
-	            }
-	        }
-	    }
+	   
 		   public void paint(Graphics g1) {
 			   //add lights!!!
 			   //for box and words!
