484244cfb0cee5d73602a866ba785a14988bf855
erinlhiggins <elh76@pitt.edu>
2017-10-12 01:10:30 -0400

    final version for class demonstration

--- src/MTR/NorthShoreExtension/UI/TrackModel.java
+++ src/MTR/NorthShoreExtension/UI/TrackModel.java
@@ -6,8 +6,6 @@ import javax.swing.*;
 import java.util.Arrays;
 
 public class TrackModel extends JPanel implements MouseListener {
-	//load csv into db
-	//test 
     class Data{
 		String linestatus; 
 		int speed;
@@ -22,23 +20,28 @@ public class TrackModel extends JPanel implements MouseListener {
 		String trackStatus;
 		String heater;
 	}
-	//read in from db here
+    protected JTextField textField;
+    protected JTextArea textArea;
+    private final static String newline = "\n";
     int x1, x2, x3, x4, y1, y2, y3, y4;
 	String status = "Free";
+	String broken = "POWER FAIL";
+	boolean broke = false;
 	Boolean clicked = false;
 	Boolean failure = false;
+	Boolean train = false;
 	String dataString = " ";
 	Point p = new Point();
-	JTable table;
-	Object[] colNames = {"Line/Status","Speed/Authority","Section","Block","Length (m)", 
+	String[] colNames = {"Line/Status","Speed/Authority","Section","Block","Length (m)", 
 			"Grade(%)", "Speed Limit (km/hr)","Infrastructre", "Elevation(m)",
 			"Cumlative Elevation", "Track Status", "Heater"};
-	//String colString = Arrays.toString(colNames).replaceAll("\\[|\\]|,|\\s", "  ");
-	//will eventually be the number of lines in a read in csv, but for now just working with the example
 	String colString = " ";
-	Object[][] data = {{"1","1","1","1","1","1","1","1","1","1","1","1"}};
 	
 	   public static void main(String[] a) {
+		   createandshowGUI();
+	   }
+	   
+	   public static void createandshowGUI() {
 		      TrackModel mouse = new TrackModel();
 		      JFrame f = new JFrame("Track Model UI");
 		      f.setSize(1200, 700);
@@ -47,7 +50,7 @@ public class TrackModel extends JPanel implements MouseListener {
 		      f.setResizable(false);
 		      f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		      f.setVisible(true);
-		   }
+	   }
 	   
 		   public void paint(Graphics g1) {
 			   //add lights!!!
@@ -62,28 +65,27 @@ public class TrackModel extends JPanel implements MouseListener {
 				  g.drawLine(75, 530, 1130, 530);
 				  g.drawLine(75, 470, 75, 530);
 				  g.drawLine(1130, 470, 1130, 530);
-				 /* g.drawLine(1060, 470, 1060, 530);
-				  g.drawLine(970, 470, 970, 530);
-				  g.drawLine(840, 470, 840, 530);
-				  g.drawLine(750, 470, 750, 530);
-				  g.drawLine(660, 470, 660, 530);
-				  g.drawLine(520, 470, 520, 530);
-				  g.drawLine(460, 470, 460, 530);
-				  g.drawLine(380, 470, 380, 530);
-				  g.drawLine(330, 470, 330, 530);
-			  	  g.drawLine(270, 470, 270, 530);
-			  	  g.drawLine(170, 470, 170, 530);*/
 			  	  g.setColor(Color.red);
 			  	  g.fillRect(550, 550, 80, 30);
 			  	  g.setColor(Color.white);
 			  	  g.drawRect(550,550,80,30);
-			  	  g.drawString("Break", 572, 570);  
+			  	  g.drawString("Break", 572, 570); 
+			  	  g.setColor(Color.black);
+			  	  if(!train||(train&&(x1!=531&&x2!=541))) {
+			  		status="Free";
+			  		g.drawString(status, 135, 520);
+			  	  }			 
 			  }
-			  
 			  g.setColor(Color.white);
 		      g.fillRoundRect (10, 10, 1178, 450, 90, 90);
 		      g.setColor(Color.black);
 		      g.drawRoundRect(10, 10, 1178, 450, 90, 90);
+			  g.setColor(Color.black);
+		      g.drawRoundRect(870, 20, 300, 50, 90, 90);
+		      g.drawString("Train Example:", 900, 40);
+		      g.fillRect(950, 45, 150, 20);
+		      g.setColor(Color.white);
+		      g.drawString("ADD TRAIN", 990, 60);
 		      g.setColor(Color.GREEN);
 		      g.setStroke(new BasicStroke(3));
 		      g.drawLine(589, 40, 589, 100);
@@ -96,18 +98,43 @@ public class TrackModel extends JPanel implements MouseListener {
 		      g.drawLine(510, 200, 600, 200);
 		      g.drawLine(610, 200, 700, 200);
 		      g.drawArc(671, 200, 75, 95, 0, 90);
+		      if(!train) {
+		    	  	g.setColor(Color.green);
+		      }
+		      else if(train) {
+		    	  	g.setColor(Color.red);
+		      }
+		      g.fillOval(595, 185, 8, 8);
 		      g.setColor(Color.green);
-		      g.fillOval(460, 185, 10, 10);
+		      g.fillOval(440, 175, 8, 8);
+		      g.fillOval(440, 215, 8, 8);
+		      g.fillOval(495, 185, 8, 8);
+		      g.fillOval(690, 185, 8, 8);
+		      g.fillOval(750, 240, 8, 8);
+		      g.fillOval(575, 40, 8, 8);
+		      g.fillOval(575, 120, 8, 8);
+		      g.fillOval(575, 230, 8, 8);
+		      
+		      g.fillRect(445, 195, 8, 8);
 		      g.setColor(Color.gray);
 		      g.fillOval(590, 150, 25, 15);
-		      g.setColor(Color.black);
-		      g.drawRoundRect(870, 20, 300, 150, 90, 90);
-		      g.drawString("Inputs for Individual Testing:", 900, 40);
 		      
 		      if(failure) {
+		    	    g.setColor(Color.black);
+		    	    g.drawString(broken, 980, 520);
 				g.setColor(Color.red);
 		    	  	g.drawLine(x1, y1, x2, y2);
 		    	  	g.drawLine(x1, y2, x2, y1);
+		      }
+		      if(train)
+		      {
+		    	  	if(clicked&&x1==531&&x2==541&&y1==194&&y2==204) {
+		    	  		g.setColor(Color.black);
+		    	  		status="Occupied";
+			    	    g.drawString(status, 135, 520);
+		    	  	}
+		    	     g.setColor(Color.blue);
+		    	     g.fillRect(540, 180, 30, 15);
 		      }
 		      
 		   }
@@ -121,6 +148,10 @@ public class TrackModel extends JPanel implements MouseListener {
 	            int y=e.getY();
 	            //note in final version will just add the row because will be preloaded
 	            //green lines
+	            if((x>=948 && x<=1103) && (y>=60 && y<=100)) {
+	            		train=!train;
+	            		repaint();
+	            }
 	            if((x>=580 && x<=600) && (y>=64 && y<=120)) {
 	            		x1=584;
 	            		x2=594;
@@ -128,7 +159,7 @@ public class TrackModel extends JPanel implements MouseListener {
 	            		y2=75;
 	            		clicked = true;
 	            		colString = Arrays.toString(colNames).replaceAll("\\[|\\]|,|\\s", "  ");
-	            		dataString = "  Green/"+status+"           55/3                A           1           100             0.5                     55                                              0.5                        0.5                                       off";
+	            		dataString = "  Green/                     55/3                A           1           100             0.5                     55                                              0.5                        0.5                                        off";
 	            		repaint();
 	            }
 	            else if((x>=580 && x<=600) && (y>=135 && y<=218)) {
@@ -138,7 +169,7 @@ public class TrackModel extends JPanel implements MouseListener {
 	            		y2=146;
 	            		clicked = true;
 	            		colString = Arrays.toString(colNames).replaceAll("\\[|\\]|,|\\s", "  ");
-	            		dataString = "  Green/"+status+"          55/3                A           2           100              1                      55                     PIONEER                  1                         1.5                                       off";
+	            		dataString = "  Green/                    55/3                A           2           100              1                      55                     PIONEER                  1                         1.5                                         off";
 	
 	            		repaint();
 	            }
@@ -148,8 +179,8 @@ public class TrackModel extends JPanel implements MouseListener {
 	            		y1=237;
 	            		y2=247;
 	            		clicked = true;
-	            		colString = Arrays.toString(colNames).replaceAll("\\[|\\]|,|\\s", "  ");
-	            		dataString = "  Green/"+status+"           55/3                A           3           100             1.5                     55                                              1.5                         3                                       off";
+	            		colString = Arrays.toString(colNames).replaceAll("\\[|\\]|,|\\s", "  "); 
+	            		dataString = "  Green/                      55/3                A           3           100             1.5                     55                                              1.5                         3                                        off";
 	
 	            		repaint();
                 }
@@ -161,7 +192,7 @@ public class TrackModel extends JPanel implements MouseListener {
 	            		y2=180;
 	            		clicked = true;
 	            		colString = Arrays.toString(colNames).replaceAll("\\[|\\]|,|\\s", "  ");
-	            		dataString = "  Red/"+status+"              55/3                A           1            50             0.5                     40                                              0.25                        0.25                                       off";
+	            		dataString = "  Red/                        55/3                A           1            50             0.5                     40                                              0.25                        0.25                                   off";
 	  
 	            		repaint();
 	            }
@@ -172,7 +203,7 @@ public class TrackModel extends JPanel implements MouseListener {
 	            		y2=222;
 	            		clicked = true;
 	            		colString = Arrays.toString(colNames).replaceAll("\\[|\\]|,|\\s", "  ");
-	            		dataString = "  Red/"+status+"              55/3                A           2            50              1                      40                                              0.50                        0.75                                       off";
+	            		dataString = "  Red/                        55/3                A           2            50              1                      40                                              0.50                        0.75                                   off";
 	   
 	            		repaint();
 	            }
@@ -183,7 +214,7 @@ public class TrackModel extends JPanel implements MouseListener {
 	            		y2=204;
 	            		clicked = true;
 	            		colString = Arrays.toString(colNames).replaceAll("\\[|\\]|,|\\s", "  ");
-	            		dataString = "  Red/"+status+"              55/3                A           3            50             1.5                     40                      SWITCH             0.75                      1.50                                       off";
+	            		dataString = "  Red/                        55/3                A           3            50             1.5                     40                      SWITCH             0.75                      1.50                                          off";
 	
 	            		repaint();
 	            }
@@ -194,7 +225,7 @@ public class TrackModel extends JPanel implements MouseListener {
 	            		y2=204;
 	            		clicked = true;
 	            		colString = Arrays.toString(colNames).replaceAll("\\[|\\]|,|\\s", "  ");
-	            		dataString = "  Red/"+status+"              55/3                B           4            50              2                      40                                              1.00                        2.50                                       off";
+	            		dataString = "  Red/                        55/3                B           4            50              2                      40                                              1.00                        2.50                                      off";
 	
 	            		repaint();
 	            }
@@ -205,7 +236,7 @@ public class TrackModel extends JPanel implements MouseListener {
 	            		y2=204;
 	            		clicked = true;
 	            		colString = Arrays.toString(colNames).replaceAll("\\[|\\]|,|\\s", "  ");
-	            		dataString = "  Red/"+status+"              55/3                B           5            50             1.5                     40                                              0.75                        3.25                                       off";
+	            		dataString = "  Red/                        55/3                B           5            50             1.5                     40                                              0.75                        3.25                                      off";
 
 	            		repaint();
 	            }
@@ -216,7 +247,7 @@ public class TrackModel extends JPanel implements MouseListener {
 	            		y2=224;
 	            		clicked = true;
 	            		colString = Arrays.toString(colNames).replaceAll("\\[|\\]|,|\\s", "  ");
-	            		dataString = "  Red/"+status+"              55/3                B           6            50              1                      40                                              0.50                        3.75                                       off";
+	            		dataString = "  Red/                        55/3                B           6            50              1                      40                                              0.50                        3.75                                     off";
 	            		repaint();
 	            }
 	            else if((x>=550 && x<=630) && (y>=550 && y<=600)) {
