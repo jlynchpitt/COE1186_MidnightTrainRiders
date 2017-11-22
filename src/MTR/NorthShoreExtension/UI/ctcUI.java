5aef7ccd54764010fb9eb06036c004474be8f9b3
jlynchpitt <joelynch523@gmail.com>
2017-11-21 21:28:57 -0400

    Revert "Fixing files that were overwritten by commits/merges revert "Merge branch 'master' of https://github.com/jlynchpitt/COE1186_MidnightTrainRiders""
    This reverts commit bc7200d5eb5f1b697c93b23c9554f90ef2f49659.

--- src/MTR/NorthShoreExtension/UI/ctcUI.java
+++ src/MTR/NorthShoreExtension/UI/ctcUI.java
@@ -38,6 +38,8 @@ import java.io.*;
 import java.util.*;
 import java.text.*;
 
+import MTR.NorthShoreExtension.UI.*;
+
 public class ctcUI {
 	
 	//Specify the look and feel to use. Valid values: null (default), "Metal", "System",
@@ -245,7 +247,7 @@ public class ctcUI {
 		trkModel.setBorder(BorderFactory.createLineBorder(Color.black,1));
 		BufferedImage image;
 		try {
-			image = ImageIO.read(new File("images/TrackLayout.png"));
+			image = ImageIO.read(new File("TrackLayout.png"));
 		} catch (IOException ex) {
 			image = null;
 		}
@@ -280,7 +282,6 @@ public class ctcUI {
 				}
 			}
 		});
-
 		pane.add(switchTest, gbc);
 	
 		schedules = new JButton("Train Schedules");
