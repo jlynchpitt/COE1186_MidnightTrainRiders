a9a2b1d95445b4b1160452c526c07ed8346e3cd5
MidoriNoHikari <mss119@pitt.edu>
2017-12-09 16:10:51 -0500

    SwitchLogic Updated
    On behalf of Eric, pushing updated switch logic for the SouthGreenLine. Trains should now be able to run anywhere betwee 2062 and 2150 with no real issues

--- src/MTR/NorthShoreExtension/Backend/WaysideController/WaysideController.java
+++ src/MTR/NorthShoreExtension/Backend/WaysideController/WaysideController.java
@@ -263,9 +263,10 @@ public class WaysideController //this class is the logic to decide what to do wi
 	public static void SouthGreenLine()
 	{
 		System.out.println("XDDDD");
-		//look at occupied tracks
+		//look at o4ccupied tracks
 		for (int x = 0; x < StackOfOccupiedTracks.size(); x++)
 		{
+			//System.out.print("OCCUPIED: " + StackOfOccupiedTracks.get(x) + " | ");
 			//get track numbers and if south green switches are added
 			if (StackOfOccupiedTracks.get(x) != 0 && SG.size() > 0)
 			{
@@ -277,14 +278,20 @@ public class WaysideController //this class is the logic to decide what to do wi
 					System.out.println("DISTANCE TO SWITCH: " + Math.abs(StackOfOccupiedTracks.get(x) - SG.get(y)));
 					
 					//if train is one track away from switch or less
-					if (Math.abs(StackOfOccupiedTracks.get(x) - SG.get(y)) <= 1 && StackOfOccupiedTracks.get(x) < SG.get(y))
+					if (Math.abs(StackOfOccupiedTracks.get(x) - SG.get(y)) <= 1)
 					{
-						//if switch is not straight
-						if (load.getSwitch(SG.get(y)) != 0)
+						System.out.println("Train is on: " + StackOfOccupiedTracks.get(x) + " and headed for switch " + SG.get(y));
+						//if the currently occupied is greater than the switch
+						if (StackOfOccupiedTracks.get(x) < SG.get(y))
 						{
-							//straighten switch
-							WaysideFunctionsHub.WaysideController_Switch(SG.get(y));
+							//if switch is not straight
+							if (load.getSwitch(SG.get(y)) != 0)
+							{
+								//straighten switch
+								WaysideFunctionsHub.WaysideController_Switch(SG.get(y));
+							}
 						}
+						
 					}
 					//if train is over one track away
 					else
@@ -298,6 +305,7 @@ public class WaysideController //this class is the logic to decide what to do wi
 					}
 				}
 			}
+			//System.out.println("");
 		}
 		/*
 		for (int x = 0; x < ListOfTrackPlans.size(); x++) //go through all the track plans
